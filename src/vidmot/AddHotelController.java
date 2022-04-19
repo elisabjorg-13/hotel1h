0b9a920e0c05d15c31e13c5d93463d3b2fd7316c
eirikuroli <eirikuroli@gmail.com>
2022-04-19 16:06:17 +0000

    searchController

--- src/vidmot/AddHotelController.java
+++ src/vidmot/AddHotelController.java
@@ -1,6 +1,38 @@
 package vidmot;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class AddHotelController {
+
+    @FXML
+    private TextField userId;
+    @FXML
+    private TextField roompriceId;
+    @FXML
+    private CheckBox petfriendlyId;
+    @FXML
+    private CheckBox familyfriendlyId;
+    @FXML
+    private TextField hotelnameId;
+    @FXML
+    private Button AddId;
+
+    private Boolean fjolskylduvaent;
+    private Boolean gaeludyr;
+
+    public void addHotelHandler(ActionEvent actionEvent) {
+        if(familyfriendlyId.isSelected()){
+            fjolskylduvaent = true;
+        }
+        if(petfriendlyId.isSelected()){
+            gaeludyr = true;
+        }
+    }
 
 
 }
